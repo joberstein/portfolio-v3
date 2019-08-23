782fc8a31e8b4048fc485e415b3dc0f778802f54
Jesse Oberstein <7157500+joberstein@users.noreply.github.com>
2019-08-23 19:41:39 -0400

    Updated websites portfolio page.

--- app/src/Portfolio/websites/data.js
+++ app/src/Portfolio/websites/data.js
@@ -1,12 +1,21 @@
 import {buildSectionData} from "PortfolioSection/component";
+import portfolioV2 from "./img/portfolio-v2.png";
 import portfolioV1 from "./img/portfolio-v1.png";
 import portfolioV0 from "./img/portfolio-v0.png";
-import bitcoin from "./img/bitcoin.png";
 import emotionMachine from "./img/emotion-machine.png";
 
 export default {
 	id: "websites",
 	data: [
+		buildSectionData("Portfolio Website v2",
+			"In this iteration of my portfolio website, I created a new design from wireframes, and " +
+			"implemented the site as a Python Flask app with Jinja templates. Since I do not currently own a hosting " +
+			"server at the moment, this website is not available for viewing (because of its Python backend).",
+			{
+				image: portfolioV2,
+				code: "https://github.com/joberstein/portfolio-v2"
+			}),
+
 		buildSectionData("Portfolio Website v1",
 			"This website was my first attempt at designing a from-scratch portfolio website. It was created using HTML, " +
 			"CSS, JavaScript and some JQuery code. Wireframes were created in Adobe InDesign, and transferred them to code. " +
@@ -15,17 +24,7 @@ export default {
 			"sticky section titles, smooth scrolling, and an expandable navigation menu.",
 			{
 				image: portfolioV1,
-				code: "https://github.com/joberstein/joberstein.github.io"
-			}),
-
-		buildSectionData("Bitcoin Website",
-			"This website was developed over the course of an entire semester for my Web Design and Development class. The " +
-			"design of the website took many iterations to bring it to where it currently stands. In creating this website, " +
-			"I worked a lot with jQuery, page animations, tooltips, and DRY methodology. It's purpose is to cater to " +
-			"audiences who may either want to learn more about Bitcoin through it's history, or find out what exactly it is.",
-			{
-				image: bitcoin,
-				link: "http://www.jesseoberstein.com/bitcoin/welcome.html"
+				link: "https://joberstein.github.com/portfolio-v1"
 			}),
 
 		buildSectionData("Emotion Machine",
@@ -34,7 +33,7 @@ export default {
 			"pastel color palette is even playful as well!",
 			{
 				image: emotionMachine,
-				link: "http://www.jesseoberstein.com/emotions/emotion-test.html"
+				link: "https://joberstein.github.com/emotion-machine"
 			}),
 
 		buildSectionData("Portfolio Website v0",
@@ -44,7 +43,7 @@ export default {
 			"in college.",
 			{
 				image: portfolioV0,
-				link: "http://www.jesseoberstein.com/portfoliov0/index.html"
+				link: "https://joberstein.github.com/portfoliov0"
 			})
 
 		// TODO need to recreate the blog
@@ -54,5 +53,16 @@ export default {
 		// 	"and was able to experience culture-shock first hand.  This blog was my effort to record everything that " +
 		// 	"happened, and I played with the CSS to make it look a lot more pleasing than it originally did.",
 		// 	{image: onward})
+
+		// TODO need to port and rebuild site
+		// buildSectionData("Bitcoin Website",
+		// 	"This website was developed over the course of an entire semester for my Web Design and Development class. The " +
+		// 	"design of the website took many iterations to bring it to where it currently stands. In creating this website, " +
+		// 	"I worked a lot with jQuery, page animations, tooltips, and DRY methodology. It's purpose is to cater to " +
+		// 	"audiences who may either want to learn more about Bitcoin through it's history, or find out what exactly it is.",
+		// 	{
+		// 		image: bitcoin,
+		// 		link: "http://www.jesseoberstein.com/bitcoin/welcome.html"
+		// 	}),
 	]
 };
