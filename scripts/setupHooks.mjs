23f43728d739ee7516331003428d0de98193ea7d
Jesse Oberstein <7157500+joberstein@users.noreply.github.com>
2022-07-11 23:27:50 -0400

    ci: Remove unneeded hooks and scripts.
    Issue 53.

--- scripts/setupHooks.mjs
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zx
-
-$.verbose = false;
-
-const projectDir = (await $`git rev-parse --show-toplevel`).stdout.trim();
-const hooksPath = `${projectDir}/hooks`;
-
-await $`git config core.hooksPath ${hooksPath}`;
-console.log(`Set local hooks path to: ${hooksPath}`);
